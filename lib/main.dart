0600dfb37a5007d5f92ddae4a9b82b802e04f7eb
FikriNurBakhtiar <ayakaaoka@gmail.com>
2023-05-13 12:01:32 +0700

    Tambah Identitas dan Janji

--- lib/main.dart
+++ lib/main.dart
@@ -57,12 +57,15 @@ class MyAppState extends State<MyApp> {
       title: 'Hello App',
       home: Scaffold(
         appBar: AppBar(
-            title: Text('My App P2P'),
-            actions: <Widget>[ButtonNamaKelompok(), ButtonPerjanjian()]),
+            title: Text('My App P2P')),
         body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                Padding(
+                  padding: EdgeInsets.all((10)),
+                  child: Text('2001123, Muhamad Fikri Nur Bakhtiar; 2009037, Ellsya Nabella Nurallifa; Saya berjanji tidak akan berbuat curang data atau membantu orang berbuat curang'),
+                ),
                 DropdownButton(
                   value: pilihanPinjaman,
                   items: pinjaman,
@@ -83,51 +86,3 @@ class MyAppState extends State<MyApp> {
 }
 
 
-class ButtonNamaKelompok extends StatelessWidget {
-  const ButtonNamaKelompok({Key? key}) : super(key: key);
-
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.account_circle_rounded),
-      onPressed: () {
-        // icon account di tap
-        showDialog<String>(
-          context: context,
-          builder: (BuildContext context) => AlertDialog(
-            title: const Text('Kelompok 18'),
-            content: const Text('Muhamad Fikri Nur Bakhtiar (fikribakhtiar62@upi.edu) ; Ellsya Nabella Nurallifa (ellsyanabella13@upi.edu)'),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () => Navigator.pop(context, 'OK'),
-                child: const Text('OK'),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
-
-
-class ButtonPerjanjian extends StatelessWidget {
-  const ButtonPerjanjian({Key? key}) : super(key: key);
-
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.access_alarm_rounded),
-      onPressed: () {
-        // icon setting ditap
-        const snackBar = SnackBar(
-          duration: Duration(seconds: 20),
-          content: Text('Kami berjanji  tidak akan berbuat curang dan atau membantu kelompok lain berbuat curang'),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      },
-    );
-  }
-}
